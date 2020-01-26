39363ec37d6e879d422e357edc567e5ae0d5f745
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2020-01-26 16:50:43 +0300

    add constants

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var wizards = [];
 var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_QUANTITY = 4;
 
-var setup = document.querySelector('.setup').classList.remove('hidden');
-var setupSimilar = document.querySelector('.setup-similar').classList.remove('hidden');
-
+var wizards = [];
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
